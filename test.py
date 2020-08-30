9adc92b7cae0892b802ace86c9e15b7e583c6f56
Sotiraki Sima <sotiraki.sima@onefirewall.com>
2020-08-30 14:29:20 +0100

    update

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ start = dt.datetime.now() - timedelta(days=2*365) #dt.datetime(20.019, 1, 1, 1)
 end = dt.datetime.now()
 
 df = web.DataReader("TLSA", 'yahoo', start, end)
-print(df.head(10))
+#print(df.head(10))
 
 
 
@@ -22,10 +22,10 @@ output1.write("open_dif\tclose_dif\tstatus\n")
 
 close_previous_day = 0.0
 
-long_on_negatives = 0.0
-short_on_negatives = 0.0
-long_on_positives = 0.0
-short_on_positives = 0.0
+long_on_negatives = [0.0,0.0]
+short_on_negatives = [0.0,0.0]
+long_on_positives = [0.0,0.0]
+short_on_positives = [0.0,0.0]
 
 for i in range(1,len(df['Close'])):
     position = "NONE"
@@ -33,16 +33,16 @@ for i in range(1,len(df['Close'])):
     if (df['Close'][i] - df['Open'][i]) > 0.0:
         position = "LONG"
         if float(df['Open'][i] - df['Close'][i-1]) < 0.0:
-            long_on_negatives += 1
+            long_on_negatives[0] += 1
         elif float(df['Open'][i] - df['Close'][i-1]) > 0.0:
-            long_on_positives += 1
+            long_on_positives[0] += 1
 
     elif (df['Close'][i] - df['Open'][i]) < 0.0:
         position = "SHORT"
         if float(df['Open'][i] - df['Close'][i-1]) < 0.0:
-            short_on_negatives += 1
+            short_on_negatives[0] += 1
         elif float(df['Open'][i] - df['Close'][i-1]) > 0.0:
-            short_on_positives += 1
+            short_on_positives[0] += 1
 
 
     output1.write(
@@ -53,10 +53,10 @@ for i in range(1,len(df['Close'])):
         position +
         "\n")
 
-print("long_on_negatives " + str(long_on_negatives/(len(df['Close']))*100))
-print("long_on_positives " + str(long_on_positives/(len(df['Close']))*100))
-print("short_on_negatives " + str(short_on_negatives/(len(df['Close']))*100))
-print("short_on_positives " + str(short_on_positives/(len(df['Close']))*100))
+print("long_on_negatives " + str(long_on_negatives[0]/(len(df['Close']))*100))
+print("long_on_positives " + str(long_on_positives[0]/(len(df['Close']))*100))
+print("short_on_negatives " + str(short_on_negatives[0]/(len(df['Close']))*100))
+print("short_on_positives " + str(short_on_positives[0]/(len(df['Close']))*100))
 
 output1.close()
 
