7d17f334ca6fafe87908321dc585812635d8b391
Sotiraki Sima <sotiraki.sima@onefirewall.com>
2020-08-31 11:02:37 +0100

    update

--- test.py
+++ test.py
@@ -9,10 +9,18 @@ import yfinance as yf
 style.use('ggplot')
 #style.use('fivethirtyeight')
 
-start = dt.datetime.now() - timedelta(days=2*365) #dt.datetime(20.019, 1, 1, 1)
+start = dt.datetime.now() - timedelta(days=10*365) #dt.datetime(20.019, 1, 1, 1)
 end = dt.datetime.now()
 
+#df = web.DataReader("AMZN", 'yahoo', start, end)
+
+#df = web.DataReader("AAPL", 'yahoo', start, end)
 df = web.DataReader("PFE", 'yahoo', start, end)
+#df = web.DataReader("GOOG", 'yahoo', start, end)
+#df = web.DataReader("TLSA", 'yahoo', start, end)
+#df = web.DataReader("JPM", 'yahoo', start, end)
+#df = web.DataReader("ZM", 'yahoo', start, end)
+
 print(df.head(10))
 
 
@@ -30,10 +38,11 @@ short_on_positives = [0.0,0.0]
 long_wins = 0.0
 short_wins = 0.0
 
+print("Trade cost: " + str((0.001 * df['Close'][0])))
 for i in range(1,len(df['Close'])):
     position = "NONE"
 
-    if (df['Close'][i] - df['Open'][i]) > 0.0:
+    if (df['Close'][i] - df['Open'][i]) > (0.001 * df['Close'][i]):
         position = "LONG"
         long_wins += float(df['Close'][i] - df['Open'][i])
         if float(df['Open'][i] - df['Close'][i-1]) < 0.0:
@@ -41,7 +50,7 @@ for i in range(1,len(df['Close'])):
         elif float(df['Open'][i] - df['Close'][i-1]) > 0.0:
             long_on_positives[0] += 1
 
-    elif (df['Close'][i] - df['Open'][i]) < 0.0:
+    elif (df['Close'][i] - df['Open'][i]) < (0.001 * df['Close'][i]):
         position = "SHORT"
         short_wins += -1*float(df['Close'][i] - df['Open'][i])
         if float(df['Open'][i] - df['Close'][i-1]) < 0.0:
@@ -62,7 +71,14 @@ print("long_on_negatives " + str(long_on_negatives[0]/(len(df['Close']))*100) + 
 print("long_on_positives " + str(long_on_positives[0]/(len(df['Close']))*100)+ ", total: " + str(long_wins))
 print("short_on_negatives " + str(short_on_negatives[0]/(len(df['Close']))*100) + ", total: " + str(short_wins))
 print("short_on_positives " + str(short_on_positives[0]/(len(df['Close']))*100) + ", total: " + str(short_wins))
-
+print("No big changes: " + str(
+            float(100 - 
+                long_on_negatives[0]/(len(df['Close']))*100 - 
+                long_on_positives[0]/(len(df['Close']))*100 - 
+                short_on_negatives[0]/(len(df['Close']))*100 - 
+                short_on_positives[0]/(len(df['Close']))*100 )
+                )
+    )
 output1.close()
 
 #df['Adj Close'].plot()
