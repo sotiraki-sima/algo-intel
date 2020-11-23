c1972b7bdb5db31eb8a44ca0beb9963a99b6597a
OneFirewall User <join@onefirewall.com>
2020-11-23 14:28:32 +0000

    d

--- alphavantage.py
+++ alphavantage.py
@@ -2,7 +2,10 @@ import requests
 import os
 import matplotlib.pyplot as plt
 from matplotlib import style
+import matplotlib 
 
+print(plt.get_backend())
+matplotlib.use('MacOSX')
 
 def keys_to_array(json_obj):
     arr = []
@@ -16,8 +19,7 @@ MAX_EXTRACT = 60
 
 url = "https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=GBP&to_symbol=EUR&outputsize=full&apikey=" + os.getenv("alpha_key")
 
-#url = "https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=GBP&to_symbol=EUR&apikey=" + os.getenv("alpha_key")
-print(url)
+#print(url)
 response = requests.get(url)
 response_json = response.json()
 
@@ -28,7 +30,7 @@ for key in response_json:
         break
 
 output2 = open("output2.csv", "w")
-output2.write("      DATE     ____-5     ____-4     ____-3     ____-2     ____-1     ____-0     POSITI     ____+1     ____DIF\n")
+output2.write("DATE,____-5,____-4,____-3,____-2,____-1,_____0,POSITI,____+1,____DIF\n")
 
 for i in range(5,-1,-1):
     print(i)
@@ -42,22 +44,21 @@ for x in range (1 ,len(trade_days)-ML_BACK_TRACK-1):
         next_day_price = float(response_json[data_key][trade_days[x+i]]["4. close"])
         today_price = float(response_json[data_key][trade_days[x+i+1]]["4. close"])
         difference_of_next_day = round(next_day_price-today_price,4)
-        output2.write("     " + str(difference_of_next_day))
+        output2.write("," + str(difference_of_next_day))
 
     next_day_price = float(response_json[data_key][trade_days[x-1]]["4. close"])
     today_price = float(response_json[data_key][trade_days[x]]["4. close"])
     difference_of_next_day = round(next_day_price-today_price,4)
     
-    
     if(difference_of_next_day>0.001):
-        output2.write("     " + str("   BUY"))
+        output2.write("," + str("BUY"))
     elif difference_of_next_day<-0.001:
-        output2.write("     " + str("  SELL"))
+        output2.write("," + str("SELL"))
     else:
-        output2.write("     " + str("NOTRAD"))
+        output2.write("," + str("NOTRAD"))
 
-    output2.write("     " + str(response_json[data_key][trade_days[x-1]]["4. close"]))
-    output2.write("     " + str(difference_of_next_day))
+    output2.write("," + str(response_json[data_key][trade_days[x-1]]["4. close"]))
+    output2.write("," + str(difference_of_next_day))
     output2.write("\n")
     '''
     output2.write(
@@ -74,32 +75,38 @@ for x in range (1 ,len(trade_days)-ML_BACK_TRACK-1):
     )
     '''
 
-#x_axis = []
-#y_axis = []
+output2.close()
 
-#cnt_max = MAX_EXTRACT
-#for key in response_json[data_key]:
-#    cnt_max -= 1
-#    if cnt_max <= MAX_EXTRACT:
-#        break
+x_axis = []
+y_axis = []
+
+cnt_max = MAX_EXTRACT
+for key in response_json[data_key]:
+    #cnt_max -= 1
+    #if cnt_max <= MAX_EXTRACT:
+    #    break
     
-
+    
     #print(key + "     " + str(float(response_json[data_key][key]["4. close"])))
 
     #for key_cursor in response_json[data_key]:
-
-    #clean_key = key.split("-")[2] + "-" + key.split("-")[1] #2020-08-31 22:00:00
-    #x_axis.insert(0, clean_key)
-    #y_axis.insert(0, float(response_json[data_key][key]["4. close"]))
     
+    clean_key = key.split("-")[2] + "-" + key.split("-")[1] #2020-08-31 22:00:00
+
+    x_axis.insert(0, key)
+    y_axis.insert(0, float(response_json[data_key][key]["4. close"]))
+        
     #print(x_axis)
     #print(y_axis)
 
+#print(x_axis)
 
-output2.close()
+print(1)
 
 
-#plt.plot(x_axis, y_axis)
-#plt.ylabel('GBP/EUR')
+x1_axis = [1,3,4]
+y1_axis = [5,6,3]
+plt.plot(x1_axis, y1_axis)
+plt.ylabel('GBP/EUR')
 
-#plt.show()
+plt.show()
---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+list_of_regex=(
+                "(eyJ)[A-Za-z0-9_=\-]{30,}\.(eyJ)[A-Za-z0-9_=\-]{20,}\.[A-Za-z0-9_=\-]{20,}"  # JWT
+                "(([Ss][Ee][Cc][Rr][Ee][Tt])|([Pp][Aa][Ss][Ss][Ww][Oo][Rr][Dd]))(\s){0,1}[=:]{1}(\s){0,1}[A-Za-z0-9_=\-]{5,}" # PASSWORD
+                "(aws_access_key_id)(\s){0,1}[=:]{0,1}(\s){0,1}[A-Za-z0-9_=\-]{20}" # aws_access_key_id = (20)
+                )
+
+for t in ${list_of_regex[@]}; do
+    grep --color=always -nrI -E --exclude-dir=.git --exclude-dir=doc --exclude-dir=./ --exclude-dir=.svn --exclude-dir=node_modules $t .
+done
